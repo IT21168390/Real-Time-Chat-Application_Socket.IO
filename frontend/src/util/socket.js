c531d424cbb5b0b084fee94fe0c832704ab58e42
IT21168390 <it21168390@my.sliit.lk>
2024-06-06 01:30:39 +0530

    updated .env SERVER_URL

--- frontend/src/util/socket.js
+++ frontend/src/util/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.REACT_APP_URL || "http://localhost:4000";
+const URL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
